2a97bf35e67dc7dd36a7886d11cf12205e82d9d5
arunmsharma <arunm2470@gmail.com>
2021-06-11 23:36:15 +0530

    updated nickname inside the database

--- src/components/dashboard/Index.js
+++ src/components/dashboard/Index.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Button, Divider, Drawer } from 'rsuite';
+import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
 
 const DashBoard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
-  const onSave = async (newData) => {
-      console.log(newData);
-  }
+  const onSave = async newData => {
+    const userNickNameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child(`name`);
+
+    try {
+      await userNickNameRef.set(newData);
+
+      Alert.success('NickName has been updated', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
 
   return (
     <>
@@ -19,11 +30,12 @@ const DashBoard = ({ onSignOut }) => {
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
         <Divider />
-        <EditableInput 
-        name="nickname"
-        initlialValue={profile.name}
-        onSave={onSave}
-        label={<h6 className="mb-2">NickName</h6>}/> 
+        <EditableInput
+          name="nickname"
+          initlialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">NickName</h6>}
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
