d090eccb77a9b4530bb2a219588a6f56b0257d86
arunmsharma <arunm2470@gmail.com>
2021-06-22 13:02:28 +0530

    Added editRoom Drawer

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -64,7 +64,7 @@ export const ProfileProvider = ({ children }) => {
           userStatusRef.off();
         }
 
-        database.ref(`./info/connected`).off();
+        database.ref(`.info/connected`).off();
 
         setProfile(null);
         setIsLoading(false);
